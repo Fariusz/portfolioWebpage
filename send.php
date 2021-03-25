7921e5a06484a19187cb787b2eadc913d36e7fbb
Radosław Loth <61850108+Fariusz@users.noreply.github.com>
2021-03-25 21:27:03 +0100

    added bugs to fix later

--- send.php
+++ send.php
@@ -1,31 +1,9 @@
- <?php
-     $to ='mrshammgod@gmail.com';
-     $from = 'mrshammgod@gmail.com';
-     $subject = 'Html Eamil Check';
-     $message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-                <html xmlns="http://www.w3.org/1999/xhtml">
-                <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>Untitled Document</title>
-                </head>
+<?php
 
-                <body>
-                <div style="width:400px; height:400px; background-color:#096; color:#FFF">
-                <h1>Welcome to inet shopping site</h1>
-                <p>
-                Dear Santosh,
-                We have received your Cash-On-Delivery order. We need to confirm your order by call. We will get in touch with you soon on Your order               will be processed only after confirmation.
-                For the payment, please pay Rs. 4000 cash to the courier person when he delivers the product.
-                "Please call us on 8882248819 to confirm after you deposit the amount."
-                </div>
-                </body>
-                </html>';
-    $headers = "From: $from\n";
-    $headers .= "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-    if(mail($to, $subject, $message, $headers)){
-        echo 'Wysłano wiadomość!';
-    }
-    else{
-        echo 'error';
-    }
+$to = 'kontakt@loth.cba.pl';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: kontakt@loth.cba.pl';
+mail($to, $subject, $message, $headers);
+
+?>
